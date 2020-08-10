4ad362ddf80a9ec7e313fba09adf20b5bb2596ca
Cullan Springstead <cullan.springstead@stardog.com>
2020-08-10 10:26:47 -0500

    bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pystardog",
-    version="0.9.6",
+    version="0.9.7",
     author="Stardog Union",
     author_email="support@stardog.com",
     description="Use Stardog with Python!",
