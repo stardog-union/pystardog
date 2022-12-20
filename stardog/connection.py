3c0e79dd9e78708fd8ebe80c02181b287410889c
Simon Cardenas <simon.cardenas@stardog.com>
2022-12-20 16:53:25 -0300

    Removes distutils warning message

--- stardog/connection.py
+++ stardog/connection.py
@@ -2,7 +2,6 @@
 """
 
 import contextlib
-import distutils.util
 
 from . import content_types as content_types
 from . import exceptions as exceptions
@@ -437,7 +436,7 @@ class Connection(object):
           >>> conn.ask('ask {:subj :pred :obj}', reasoning=True)
         """
         r = self.__query(query, "query", content_types.BOOLEAN, **kwargs)
-        return bool(distutils.util.strtobool(r.decode()))
+        return r.decode().lower() == "true"
 
     def update(self, query, **kwargs):
         """Executes a SPARQL update query.
@@ -473,7 +472,7 @@ class Connection(object):
             params={"graph-uri": graph_uri},
         )
 
-        return bool(distutils.util.strtobool(r.text))
+        return r.text.lower() == "true"
 
     def explain_inference(self, content):
         """Explains the given inference results.
@@ -731,7 +730,7 @@ class ICV(object):
                 params={"graph-uri": graph_uri},
             )
 
-            return bool(distutils.util.strtobool(r.text))
+            return r.text.lower() == "true"
 
     def explain_violations(self, content, graph_uri=None):
         """
