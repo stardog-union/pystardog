609959ded28d892a9263fa8eb4bd311c2a271494
Noah Gorstein <noah.gorstein@stardog.com>
2025-08-08 13:59:20 -0400

    bump version to v0.19.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2019-2025 Stardog Union"
 author = "Stardog Union"
 
 # The short X.Y version
-version = "0.18.1"
+version = "0.19.0"
 # The full version, including alpha/beta/rc tags
 release = ""
 
